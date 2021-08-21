f5c47149f986a598f68f6c5ac95c1432e53885ea
殇紫 <724674401@qq.com>
2021-08-21 16:37:09 +0800

    49

---
+++ Solutions/49.字母异位词分组.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using System;
+/*
+ * @lc app=leetcode.cn id=49 lang=csharp
+ *
+ * [49] 字母异位词分组
+ */
+
+// @lc code=start
+public partial class Solution {
+    public IList<IList<string>> GroupAnagrams(string[] strs) {
+            var di=new Dictionary<string,List<string>>();
+            char[] tem;
+            var res=new List<IList<string>>();
+            var k=string.Empty;
+            foreach(var i in strs){
+                tem=i.ToCharArray();
+                Array.Sort(tem);
+                k=string.Join("",tem);
+                if (di.ContainsKey(k)){
+                    di[k].Add(i);
+                } else{
+                    di.Add(k,new List<string>{i});
+                }
+            }
+           foreach(var key in di.Keys){
+               res.Add(di[key]);
+           }
+            return res;
+
+    }
+}
+// @lc code=end
+
