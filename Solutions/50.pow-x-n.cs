8ca258406cf3e217df977354126d1f67632f98a0
殇紫 <724674401@qq.com>
2021-08-21 22:54:17 +0800

    50

---
+++ Solutions/50.pow-x-n.cs
@@ -0,0 +1,54 @@
+using System;
+/*
+ * @lc app=leetcode.cn id=50 lang=csharp
+ *
+ * [50] Pow(x, n)
+ */
+
+// @lc code=start
+public partial class Solution
+{
+    public double MyPow(double x, int n)
+    {
+        var tem = x;
+        double result = 1;
+        int isNegative = 0;
+        if (n < 0)
+        {
+            if (n == int.MinValue)
+            {
+                n = int.MaxValue;
+                isNegative = 2;
+            }
+            else
+            {
+                n *= -1;
+                isNegative = 1;
+            }
+
+
+        }
+        for (; n > 0; n /= 2)
+        {
+            if (n % 2 == 1)
+            {
+
+                result *= tem;
+
+            }
+            tem *= tem;
+
+        }
+
+        if (isNegative == 1)
+        { 
+            return 1 / result; 
+        }
+        else if (isNegative==2){
+            return 1 / (result*x);
+        }
+        return result;
+    }
+}
+// @lc code=end
+
