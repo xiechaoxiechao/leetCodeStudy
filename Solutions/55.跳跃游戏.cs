eb21336845a2e2b9248130cf52e9041c9b3c5c08
殇紫 <724674401@qq.com>
2021-08-23 12:13:20 +0800

    55

---
+++ Solutions/55.跳跃游戏.cs
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=55 lang=csharp
+ *
+ * [55] 跳跃游戏
+ */
+
+// @lc code=start
+public partial class Solution {
+    public bool CanJump(int[] nums) {
+        int max;
+        int tem;
+        if (nums.Length==1){
+            return true;
+        }
+        for(int i=0;i<nums.Length;){
+            tem=0;
+            max=0;
+            if(i+nums[i]>=nums.Length-1){
+                return true;
+            }
+            for (int j = 1; j <= nums[i]; j++)
+            {
+                if(max<j+nums[j+i]){
+                    max=j+nums[j+i];
+                    tem=j;
+                }
+            }
+            if (tem==0){
+                return false;
+            }
+            i+=tem;
+        }
+        return false;
+    }
+}
+// @lc code=end
+
