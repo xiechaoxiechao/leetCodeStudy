1368f878a9964f83fb89066fa45d8b7bc7b1f7e9
殇紫 <724674401@qq.com>
2021-08-24 22:56:44 +0800

    58

---
+++ Solutions/58.最后一个单词的长度.cs
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=58 lang=csharp
+ *
+ * [58] 最后一个单词的长度
+ */
+
+// @lc code=start
+public partial class Solution {
+    public int LengthOfLastWord(string s) {
+        var index=s.LastIndexOf(' ');
+        if(index==s.Length-1){
+            var len=0;
+            for(--index;index>=0&&s[index]==' ';--index){};
+            for(;index>=0&&s[index]!=' ';--index){
+                ++len;
+            }
+            return len;
+        }else{
+            return s.Length-1-index;
+        }
+       
+
+    }
+}
+// @lc code=end
+
