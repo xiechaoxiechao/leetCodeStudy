b04fb657780bc8c67d93b12e88466a88f05f575d
殇紫 <724674401@qq.com>
2021-08-24 10:22:03 +0800

    56

--- Solutions/56.合并区间.cs
+++ Solutions/56.合并区间.cs
@@ -7,31 +7,42 @@ using System.Collections.Generic;
  */
 
 // @lc code=start
-public partial class Solution {
-    public int[][] Merge(int[][] intervals) {
-        Array.Sort(intervals,(t1,t2)=>{
-            if (t1[0]<t2[0]){
+public partial class Solution
+{
+    public int[][] Merge(int[][] intervals)
+    {
+        Array.Sort(intervals, (t1, t2) =>
+        {
+            if (t1[0] < t2[0])
+            {
                 return -1;
-            }else if (t1[0]>t2[0]){
+            }
+            else if (t1[0] > t2[0])
+            {
                 return 1;
-            }else{
-                 return 0;
+            }
+            else
+            {
+                return 0;
             }
         });
-        var res=new List<int[]>();
-        for(int i=0;i<intervals.Length-1;++i){
-            if(intervals[i][1]<intervals[i+1][0]){
-               
-                    res.Add(intervals[i]);
-    
-                
-            }else if(intervals[i][1]>=intervals[i+1][1]){
-                    intervals[i+1]=intervals[i];
-            }else{
-            intervals[i+1][0]=intervals[i][0];
+        var res = new List<int[]>();
+        for (int i = 0; i < intervals.Length - 1; ++i)
+        {
+            if (intervals[i][1] < intervals[i + 1][0])
+            {
+                res.Add(intervals[i]);
+            }
+            else if (intervals[i][1] >= intervals[i + 1][1])
+            {
+                intervals[i + 1] = intervals[i];
+            }
+            else
+            {
+                intervals[i + 1][0] = intervals[i][0];
             }
         }
-        res.Add(intervals[intervals.Length-1]);
+        res.Add(intervals[intervals.Length - 1]);
         return res.ToArray();
 
     }
