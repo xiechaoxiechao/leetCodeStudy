417ba8a95049b3704c884484cc35cc0d49691735
殇紫 <724674401@qq.com>
2021-08-26 17:37:13 +0800

    66

---
+++ Solutions/66.加一.cs
@@ -0,0 +1,32 @@
+using System;
+/*
+ * @lc app=leetcode.cn id=66 lang=csharp
+ *
+ * [66] 加一
+ */
+
+// @lc code=start
+public partial class Solution {
+    public int[] PlusOne(int[] digits) {
+        Action<int> ana=null;
+        ana=(index)=>{
+            if(digits[index]==9){
+                digits[index]=0;
+                if(index!=0){
+                    ana(index-1);
+                }else{
+                   Array.Resize(ref digits,digits.Length+1);
+                   digits[0]=1;
+                }
+                
+            }else{
+                ++digits[index];
+            }
+        };
+        ana(digits.Length-1);
+        
+        return digits;
+    }
+}
+// @lc code=end
+
