1f5b355572a1a446c02bcf3563a016ebc504d368
殇紫 <724674401@qq.com>
2021-08-27 12:14:26 +0800

    69

---
+++ Solutions/69.x-的平方根.cs
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=69 lang=csharp
+ *
+ * [69] x 的平方根
+ */
+
+// @lc code=start
+public partial class Solution {
+    public int MySqrt(int x) {
+        double c=x;
+        while(c*c-x>=1){
+            c=c/2+x/(2*c);
+            
+        }
+        return (int)c;
+        
+
+    }
+}
+// @lc code=end
+
