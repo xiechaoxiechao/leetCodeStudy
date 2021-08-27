f25141b9c7aca92c731bc4ecc010714220e4d8c5
殇紫 <724674401@qq.com>
2021-08-27 18:38:44 +0800

    70

---
+++ Solutions/70.爬楼梯.cs
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=70 lang=csharp
+ *
+ * [70] 爬楼梯
+ */
+
+// @lc code=start
+public partial  class Solution {
+    public int ClimbStairs(int n) {
+        if (n==1){
+            return 1;
+        }
+        if(n==2){
+            return 2;
+        }
+        var last1=1;
+        var last2=2;
+        var res=0;
+        for(int i=3;i<=n;++i){
+            res=last1+last2;
+            last1=last2;
+            last2=res;
+        }
+        return res;
+
+    }
+}
+// @lc code=end
+
