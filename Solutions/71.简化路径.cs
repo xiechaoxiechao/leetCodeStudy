1003165543d0d251da3eeb49a75187eb77f06c30
殇紫 <724674401@qq.com>
2021-08-27 19:25:26 +0800

    71

---
+++ Solutions/71.简化路径.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+/*
+ * @lc app=leetcode.cn id=71 lang=csharp
+ *
+ * [71] 简化路径
+ */
+
+// @lc code=start
+public partial class Solution {
+    public string SimplifyPath(string path) {
+        var tem=new List<string>(path.Split('/',System.StringSplitOptions.RemoveEmptyEntries));
+        tem.RemoveAll((s)=>s==".");
+        for(var i=tem.IndexOf("..");i!=-1;i=tem.IndexOf("..")){
+            if(i!=0){
+                tem.RemoveRange(i-1,2);
+            }else{
+                tem.RemoveAt(i);
+            }
+            
+        }
+
+        return "/"+string.Join("/",tem);
+         
+
+    }
+}
+// @lc code=end
+
