7212e8ed1cbc7aa8e8e616c7845c206c8129ba76
shangzi <724674401@qq.com>
2022-04-26 10:20:18 +0800

    220426

---
+++ Solutions/883.三维形体投影面积.go
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=883 lang=golang
+ *
+ * [883] 三维形体投影面积
+ */
+
+// @lc code=start
+package Solutions
+
+func projectionArea(grid [][]int) int {
+	var l = len(grid)
+	var area = l * l
+	for i := 0; i < l; i++ {
+		max := 0
+		max2 := 0
+		for j := 0; j < l; j++ {
+			if max < grid[i][j] {
+				max = grid[i][j]
+			}
+			if max2 < grid[j][i] {
+				max2 = grid[j][i]
+			}
+		}
+		area += max
+		area += max2
+
+	}
+	return area
+}
+
+// @lc code=end
