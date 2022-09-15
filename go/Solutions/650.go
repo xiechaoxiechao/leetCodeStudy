bded90067e323de0dd3b61f1349b533a9899c541
殇紫 <724674401@qq.com>
2022-09-15 07:24:09 +0000

    add go/Solutions/650.go.
    Signed-off-by: 殇紫 <724674401@qq.com>

---
+++ go/Solutions/650.go
@@ -0,0 +1,32 @@
+package Solutions
+
+func minSteps(n int) int {
+	num := 1
+	mem := 1
+	if n == 1 {
+		return 0
+	}
+	return dfs(1, num, n, mem)
+
+}
+
+func dfs(step int, num int, aim int, mem int) int {
+
+	step1, step2 := math.MaxInt32, math.MaxInt32
+	if num+mem == aim {
+		return step + 1
+	} else if num+mem < aim {
+		step1 = dfs(step+1, num+mem, aim, mem)
+	} else {
+		return math.MaxInt32
+	}
+
+	if mem != num {
+		step2 = dfs(step+1, num, aim, num)
+	}
+	if step1 < step2 {
+		return step1
+	}
+	return step2
+
+}
