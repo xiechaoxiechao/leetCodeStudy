0f8247d21d3e4946849c65f8198007cafbfdafd8
殇紫 <724674401@qq.com>
2022-09-19 07:50:01 +0000

    update go/Solutions/面试题16.17.go.
    Signed-off-by: 殇紫 <724674401@qq.com>

---
+++ go/Solutions/面试题16.17.go
@@ -0,0 +1,25 @@
+package Solutions 
+
+
+func maxSubArray(nums []int) int {
+	var dp = make([]int, len(nums))
+	for i := range nums {
+		dp[i] = nums[i]
+	}
+	for i := 1; i < len(nums); i++ {
+		dp[i] = max(dp[i-1]+dp[i], dp[i])
+	}
+	max := math.MinInt32
+	for _, v := range nums {
+		if v > max {
+			max = v
+		}
+	}
+	return max
+}
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
