a73edfedbd0ea4b540ebfe2b7c7b6c3b693360f2
殇紫 <724674401@qq.com>
2022-09-21 09:31:21 +0000

    add go/Solutions/455.go.
    Signed-off-by: 殇紫 <724674401@qq.com>

---
+++ go/Solutions/455.go
@@ -0,0 +1,18 @@
+package Solutions 
+
+
+func findContentChildren(g []int, s []int) int {
+	sort.Ints(g)
+	sort.Ints(s)
+	var ans = 0
+	for i, j := 0, 0; i < len(g) && j < len(s); {
+		if g[i] <= s[j] {
+			ans++
+			i++
+			j++
+		} else {
+			j++
+		}
+	}
+	return ans
+}
