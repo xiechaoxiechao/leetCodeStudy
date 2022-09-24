5c9ec05f066033c41bd40736f7cb922c56ed7a07
殇紫 <724674401@qq.com>
2022-09-24 04:54:58 +0000

    add go/Solutions/1370.go.
    Signed-off-by: 殇紫 <724674401@qq.com>

---
+++ go/Solutions/1370.go
@@ -0,0 +1,41 @@
+package Solutions
+
+func sortString(s string) string {
+	mp := map[byte]int{}
+	for i := 0; i < len(s); i++ {
+		mp[s[i]]++
+	}
+	var byteKeys = make([]byte, 0, len(mp))
+	for k := range mp {
+		byteKeys = append(byteKeys, k)
+	}
+	sort.Slice(byteKeys, func(i, j int) bool {
+		return byteKeys[i] < byteKeys[j]
+	})
+	var ans = make([]byte, 0, len(s))
+	for len(mp) != 0 {
+		for i := 0; i < len(byteKeys); i++ {
+			if v, ok := mp[byteKeys[i]]; ok {
+				ans = append(ans, byteKeys[i])
+				if v--; v != 0 {
+					mp[byteKeys[i]] = v
+				} else {
+					delete(mp, byteKeys[i])
+				}
+			}
+		}
+		for i := len(byteKeys) - 1; i > -1; i-- {
+			if v, ok := mp[byteKeys[i]]; ok {
+				ans = append(ans, byteKeys[i])
+				if v--; v != 0 {
+					mp[byteKeys[i]] = v
+				} else {
+					delete(mp, byteKeys[i])
+				}
+			}
+		}
+
+	}
+	return string(ans)
+
+}
