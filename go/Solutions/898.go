9408282d0b9fdd80be8914d35d426c8a97265481
殇紫 <724674401@qq.com>
2022-09-27 05:43:30 +0000

    add go/Solutions/898.go.
    Signed-off-by: 殇紫 <724674401@qq.com>

---
+++ go/Solutions/898.go
@@ -0,0 +1,22 @@
+package Solutions
+
+
+func subarrayBitwiseORs(arr []int) int {
+	var mp1 = make(map[int]struct{})
+	mp1[arr[0]] = struct{}{}
+	var mp = make(map[int]struct{}, 32)
+	mp[arr[0]] = struct{}{}
+	for i := 1; i < len(arr); i++ {
+		dpT := make(map[int]struct{})
+		dpT[arr[i]] = struct{}{}
+		mp[arr[i]] = struct{}{}
+		for k, _ := range mp1 {
+			t := k | arr[i]
+			mp[t] = struct{}{}
+			dpT[t] = struct{}{}
+		}
+		mp1 = dpT
+     
+	}
+	return len(mp)
+}
