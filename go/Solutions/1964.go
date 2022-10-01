ef782a65ff57580d18b68ed8eabff67cf63e7350
殇紫 <724674401@qq.com>
2022-10-01 07:42:57 +0000

    add go/Solutions/1964.go.
    Signed-off-by: 殇紫 <724674401@qq.com>

---
+++ go/Solutions/1964.go
@@ -0,0 +1,51 @@
+package Solutions
+
+
+func reformatNumber(number string) string {
+	number = strings.ReplaceAll(strings.ReplaceAll(number, " ", ""), "-", "")
+	sb := strings.Builder{}
+	length := len(number)
+	for i := 0; i < length;  {
+		if length-i > 4 {
+
+			sb.WriteByte(number[i])
+			i++
+			sb.WriteByte(number[i])
+			i++
+			sb.WriteByte(number[i])
+			i++
+            sb.WriteByte('-')
+			continue
+		}
+		if length-i == 4 {
+			sb.WriteByte(number[i])
+			i++
+			sb.WriteByte(number[i])
+			i++
+			sb.WriteByte('-')
+			sb.WriteByte(number[i])
+			i++
+			sb.WriteByte(number[i])
+			i++
+			break
+		}
+		if length-i == 3 {
+			sb.WriteByte(number[i])
+			i++
+			sb.WriteByte(number[i])
+			i++
+			sb.WriteByte(number[i])
+			i++
+			break
+		}
+		if length-i == 2 {
+			sb.WriteByte(number[i])
+			i++
+			sb.WriteByte(number[i])
+			i++
+			break
+		}
+
+	}
+	return sb.String()
+}
