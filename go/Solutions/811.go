70cb337bd6f922cb0b6c04101bf22dc896509b4c
殇紫 <724674401@qq.com>
2022-10-05 03:50:15 +0000

    add go/Solutions/811.go.
    Signed-off-by: 殇紫 <724674401@qq.com>

---
+++ go/Solutions/811.go
@@ -0,0 +1,24 @@
+package Solutions
+
+
+func subdomainVisits(cpdomains []string) []string {
+	var mp = make(map[string]int)
+	for i := 0; i < len(cpdomains); i++ {
+		spaceIndex := strings.IndexByte(cpdomains[i], ' ')
+		count, _ := strconv.Atoi(cpdomains[i][:spaceIndex])
+		domain := cpdomains[i][spaceIndex+1:]
+        mp[domain]+=count
+		for strings.Count(domain, ".") > 0 {
+			dotIndex := strings.IndexByte(domain, '.')
+			domain = domain[dotIndex+1:]
+			mp[domain] += count
+		}
+       
+
+	}
+	var ans = make([]string,0, len(mp))
+	for k, v := range mp {
+		ans = append(ans, strconv.Itoa(v)+" "+k)
+	}
+	return ans
+}
